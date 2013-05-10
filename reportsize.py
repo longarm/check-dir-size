2102108dfe9827124e632d180e965be32b936e37
longarm <chenmichael0@gmail.com>
2013-05-10 23:00:29 +0800

    Update reportsize.py

--- reportsize.py
+++ reportsize.py
@@ -6,17 +6,16 @@ import string
 def reportSize():
 
     #content = os.popen('du -sh *').read() 
-    content = ''' 11G  a/b/v/d  alpha
+    content = '''    11G    a/b/v/d    alpha
     
-                  111G a/c/d/e beta
-                  
-                  150m  a/bd/dfe delta
-                  
-                  '''
+                    111G    a/c/d/e    beta
+                    150m    a/bd/dfe    delta
+                    
+                    '''
     lines = content.split('\n')
     for line in lines:
         if(line.strip() != ''):
-            tokens = line.split(' ')
+            tokens = line.split('\t')
             tokens = filter(None, tokens)
             oSize = tokens[0]
             length = len(oSize)
